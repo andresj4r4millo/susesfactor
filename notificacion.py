180a043074b3d816a3c64b26beb79064f57d6240
andresj4r4millo <134433622+andresj4r4millo@users.noreply.github.com>
2023-08-25 17:27:23 -0500

    notificacion

--- notificacion.py
+++ notificacion.py
@@ -1,70 +1,128 @@
 from notifypy import Notify
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
-import pyautogui
-import pygetwindow as gw
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
 from notifypy import Notify
-"""
-notification = Notify()
-notification.title = "Cool Title"
-notification.message = "Even cooler message."
+import re
+import datetime
+import pymysql
 
-notification.send()
-"""
-def buscar(anime):
-    buscador=driver.find_element(By.XPATH,'//*[@id="searchInput"]')
-    buscador.clear()
-    buscador.send_keys(anime)
-    time.sleep(2)
-    buscador.send_keys(Keys.ENTER)
-    time.sleep(2)
-animes= {
-    "kenichi": False,
-    "boku no hero": False,
-    "hajime no ippo":False,
-    "jojos":False,
-    "wabi wabo":False
-}
 driver= webdriver.Edge()
 driver.maximize_window()
+
+
+tokens={}
 # Abrir una página web en el navegador
-driver.get("https://www.wikipedia.org/")
-time.sleep(2)
-for i in animes:
-    #anime=i.keys()
-    #print(i)
-    buscar(i)
-    #/html/body/div[2]/div/div/main/ul
-    if i in driver.page_source:
-    #try:
-        #driver.find_element(By.XPATH,'/html/body/div[2]/div/div/main/div/ul/li/a')
-        time.sleep(1)
-        screenshot_name = f'capturas/{i}.png'
-        driver.save_screenshot(screenshot_name)
-        print(f"Captura tomada y guardada en {screenshot_name}")
-        print("captura")
-        # Llamar a la función para enviar el correo
-        #enviar_correo(i)
-        notification = Notify()
-        notification.title = "Cool Title"
-        notification.message = "Even cooler message."
+driver.get("https://messages.google.com/web/conversations/20")
+while True:
+    print("MENU DE OPCIONES: ")
 
-        notification.send()
-        with open('animes encontrados.txt', 'w') as archivo:
-
-            archivo.write(i + '\n')
-        print("se encontro")
+    print("digite 1 para hogar ")
+    print("digite 2 para movil ")
+    print("digite 3 para portabilidad ")
+    print("digite 4 para migracion ")
+    print("digite 5 para grupo elite ")
+    print("digite 6 para comunidad ")
+    print("digite 7 para uraba ")
+    print("digite 8 para backoffice ")
+    opc=input("digite opcion de campaña: ")
+    if opc==1 or opc=='1':
+        campaña="hogar"
+        break
+    elif opc==2 or opc=='2':
+        campaña="movil"
+        break
+    elif opc==3 or opc=='3':
+        campaña="portabilidad"
+        break
+    elif opc==4 or opc=='4':
+        campaña="migracion"
+        break
+    elif opc==5 or opc=='5':
+        campaña="elite"
+        break
+    elif opc==6 or opc=='6':
+        campaña="comunidad"
+        break
+    elif opc==7 or opc=='7':
+        campaña="uraba"
+    elif opc==8 or opc=="8":
+        campaña=="back"
     else:
-        print("no activo")
-   # time.sleep(20)
+        print("opcion no valida, vuelva a digitar ")
+        continue
 
-    # Hacer más acciones con el navegador...
+while True:
 
-    # Cerrar el navegador
+    opc=input("estas dentro?: ")
+    if opc=="si" or opc=="SI" or opc=="Sí":
+        opc="si"
+        break
+    else:
+        continue
+if opc=="si":
+    while True:
+        try:
+            reciente=driver.find_element(By.XPATH,'/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-main-nav/mws-conversations-list/nav/div[1]/mws-conversation-list-item[1]')
+            reciente.click()
+            sms=driver.find_element(By.XPATH,'/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-conversation-container/div/div[1]/div/mws-messages-list/mws-bottom-anchored')
+            texto=reciente.text
+            patron = r'\d{10}'
+
+            txt = re.sub(r'[^0-9]', '', texto)
+
+            remitente = txt[:10]
+
+            # Corta los siguientes 6 dígitos y almacénalos en otra variable
+            codigo = txt[10:16]
+            if remitente=="3242078956":
+                if codigo not in tokens:
+                    hora_actual = datetime.datetime.now()
+                    la_hora=hora_actual.strftime('%H:%M')
+                    tokens[codigo]=la_hora
+                    hora=tokens[codigo]
+                    notification = Notify()
+                    notification.title = "token nuevo"
+                    notification.message = f"Token agregado a la lista:\nCódigo: {codigo}\nHora: {hora}"
+                    notification.send()
+                    ############
+                    try:
+                        conn = pymysql.connect(
+                            host="10.206.69.198",
+                            port=12125,
+                            user="mysqldb",
+                            password="Colombia2025=",
+                            database="token"
+                        )
+                        cursor = conn.cursor()
+
+                        # Sentencia SQL de inserción
+                        sql = "INSERT INTO tokens (campaña, codigo, hora) VALUES (%s, %s, %s)"
+
+                        # Valores a insertar
+                        valores = (campaña, codigo, hora)
+
+                        # Ejecutar la sentencia SQL
+                        cursor.execute(sql, valores)
+
+                        # Confirmar la transacción
+                        conn.commit()
+
+                        print("mensaje recibido")
+                    except Exception as e:
+                        print(f"Error: {e}")
+
+                    finally:
+                        # Cerrar el cursor y la conexión
+                        if cursor:
+                            cursor.close()
+                        if conn:
+                            conn.close()
+
+                    ############
+
+        except Exception as e:
+            print(f"no se pudo interactuar error: {e}")
+        # Imprime el texto
     
+
--- prueba2.py
+++ prueba2.py
@@ -1,4 +1,59 @@
-fecha = "02/04/2023"
-fechas = fecha.split("/")
-fechan = "".join(fechas)
-print(fechan)
+import re
+import datetime
+import pymysql
+
+
+# Texto de ejemplo
+texto = "324 2078956 Lorena ajdjvdgaks jsjdhsjjad Jsjsjsjs jdjdjdjjd zkdijdhdud 35753888 13 min"
+
+# Busca los primeros 10 números en el texto
+patron = r'\d{10}'
+txt = re.sub(r'[^0-9]', '', texto)
+
+primeros_10 = txt[:10]
+
+# Corta los siguientes 6 dígitos y almacénalos en otra variable
+codigo = txt[10:16]
+campaña="hogar"
+
+# Imprime las variables resultantes
+print("Primeros 10 dígitos:", primeros_10)
+print("Siguientes 6 dígitos:", codigo)
+hora_actual = datetime.datetime.now()
+hora = hora_actual.strftime('%H:%M')
+
+# Imprime la hora formateada
+print(hora)
+try:
+    conn = pymysql.connect(
+        host="10.206.69.198",
+        port=12125,
+        user="mysqldb",
+        password="Colombia2025=",
+        database="token"
+    )
+    cursor = conn.cursor()
+
+    # Sentencia SQL de inserción
+    sql = "INSERT INTO tokens (campaña, codigo, hora) VALUES (%s, %s, %s)"
+
+    # Valores a insertar
+    valores = (campaña, codigo, hora)
+
+    # Ejecutar la sentencia SQL
+    cursor.execute(sql, valores)
+
+    # Confirmar la transacción
+    conn.commit()
+
+    print("Sentencia SQL ejecutada con éxito")
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    # Cerrar el cursor y la conexión
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
+
